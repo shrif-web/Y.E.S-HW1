e4ed7791c48dcbb9e8065bf4f58484d5f4299bdd
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-24 19:06:20 +0330

    adding works perfectly!

--- nodejs/sha256.js
+++ nodejs/sha256.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = 8080;
 
 app.use(cors());
-app.get('/nodejs/sha256', (req, res) => {
-  res.json({ result: 12 });
-  console.log(res);
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/nodejs/sha256',function(req,res){
+  res.json({
+    result: Number(req.body.f)+Number(req.body.s)
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
