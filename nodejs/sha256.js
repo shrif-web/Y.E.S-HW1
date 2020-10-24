81ceebb558e77577df46b6d7456a734cf91ea99f
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-24 19:11:32 +0330

    smooth refactor

--- nodejs/sha256.js
+++ nodejs/sha256.js
@@ -3,17 +3,19 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 8080;
+const domain = "http://localhost"
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/nodejs/sha256',function(req,res){
+  console.log("request body:",req.body);
   res.json({
-    result: Number(req.body.f)+Number(req.body.s)
+    result: Number(req.body.first)+Number(req.body.second)
   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`nodejs app listening at ${domain}:${port}`)
 });
