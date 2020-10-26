559c8c980154dadd61077e506c4e203165383aca
mohammadrezaabdi <wnick2085@gmail.com>
2020-10-26 13:22:34 +0330

    better go backend implementation

--- go/main.go
+++ go/main.go
@@ -1,54 +1,58 @@
 package main
 
 import (
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 type DualNum struct{
-	A0 int `db:"a0" json:"a0,string"`
-	A1 int `db:"a1" json:"a1,string"`
+	A0 int `json:"first"`
+	A1 int `json:"second"`
 }
 
-// request example :
-//{
-//    "a0": "2",
-//    "a1": "3"
-//}
-
 func main() {
-	http.HandleFunc("/", handle_func)
+	http.HandleFunc("/", reqHandler)
+	http.HandleFunc("/go/sha256", sha256Response)
+	http.HandleFunc("/go/write", lineSenderResponse)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handle_func(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/go/sha256" {
-		http.Error(w, "fuck you", http.StatusNotFound)
+func reqHandler(w http.ResponseWriter, r * http.Request){
+	if r.URL.Path != "/go/sha256" && r.URL.Path != "/go/write" {
+		http.Error(w, "404 Error", http.StatusNotFound)
 		return
 	}
+}
+
+func lineSenderResponse(w http.ResponseWriter, r *http.Request){
+	enableCors(&w)
+	//todo
+}
+
+func sha256Response(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	switch r.Method {
 	case "POST":
+		bod, err := ioutil.ReadAll(r.Body)
 		var nums DualNum
-		err := json.NewDecoder(r.Body).Decode(&nums)
+		err = json.Unmarshal(bod, &nums)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		log.Println("sum result = ", nums.A0 + nums.A1)
-		hasher := sha1.New()
+		hasher := sha256.New()
 		hasher.Write([]byte(strconv.Itoa(nums.A0 + nums.A1)))
-		//sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil)) //todo which one ?
 		sha := hex.EncodeToString(hasher.Sum(nil))
 		fmt.Fprintf(w, "Result: %s", sha)
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
-		//io.WriteString(w, "Sorry, only GET and POST methods are supported.")
 	}
 }
 
