b4bf9717c1f1a4784fc8144c3c80f695e23b0007
mohammadrezaabdi <wnick2085@gmail.com>
2020-10-26 19:31:51 +0330

    file-IO js backend handling

---
+++ nodejs/main.js
@@ -0,0 +1,38 @@
+const sha256 = require('js-sha256');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 8080;
+const domain = "http://localhost"
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/nodejs/sha256', function (req, res) {
+  console.log("request body:", req.body);
+  res.json({
+    result: sha256((Number(req.body.first) + Number(req.body.second)).toString())
+  });
+});
+
+app.get('/nodejs/write', function (req, res) {
+  console.log("request body:", req.body);
+  if (!(req.body.number > 0 && req.body.number <= 100)) {
+    res.json({
+      error: "input number range should be between 1-100"
+    });
+    return
+  }
+  var kharkir = fs.readFileSync('my_text.txt', 'utf8') 
+  res.json({
+    result: kharkir.split("\n")[req.body.number-1]
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`nodejs app listening at ${domain}:${port}`)
+});
