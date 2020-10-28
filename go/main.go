6776abebc361d6e4fb7534fc03d064c357381d42
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-28 23:18:41 +0330

    removing extra logs

--- go/main.go
+++ go/main.go
@@ -52,11 +52,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func readFile(lineNumber int) (string, error) {
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("path:"+path)  // for example /home/user
 	file, err := os.Open("go/test.txt")
 	if err != nil {
 		log.Printf("failed opening file: %s", err)
