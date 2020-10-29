aba5e16bcf103be9e5cd3b29fdc7a1a6fa465cc3
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-29 21:33:22 +0330

    adding sysctl config

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,22 +1,31 @@
 import time
-from locust import HttpUser, task, constant
+import random
+from locust import HttpUser, task, constant , tag
 
+max_line=3
 class GoUser(HttpUser):
-    wait_time = constant(0.5)
+    wait_time = constant(1)
+
+    @tag('go')
     @task
     def sha(self):
         self.client.post("/go/sha256", json={"first":"2", "second":"3"})
+
+    @tag('go')
     @task
     def write(self):
-        self.client.get("/go/write?number=1")
+        self.client.get("/go/write?number={}".format(random.randint(1,max_line)),name="/go/write")
 
 
 class NodeUser(HttpUser):
-    wait_time = constant(0.5)
+    wait_time = constant(1)
+    @tag('node')
     @task
     def sha(self):
         self.client.post("/nodejs/sha256", json={"first":"2", "second":"3"})
+
+    @tag('node')
     @task
     def write(self):
-        self.client.get("/nodejs/write?number=1")
+        self.client.get("/nodejs/write?number={}".format(random.randint(1,max_line)),name="/node/write")
 
